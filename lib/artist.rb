5c6a93cc03bc395167a52ac89dec8546b540fc5e
Justin Winger <justinwinger4@gmail.com>
2020-04-06 16:45:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,34 @@
 require 'pry'
-class Artist
-
-  attr_accessor :name, :songs
-
+class Artist 
+  attr_accessor :name :songs
   @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = []
   end
+  
 
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
-
+  
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count += 1 
+    # binding.pry
   end
-
+  
+  def songs 
+    @songs 
+  end
+  
   def self.song_count
     @@song_count
   end
-
-end
+  
+end 
