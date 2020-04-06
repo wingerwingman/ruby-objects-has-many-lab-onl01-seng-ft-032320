cffa0e7ce692528e3ad67378764d67278e31cb31
Justin Winger <justinwinger4@gmail.com>
2020-04-06 15:49:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,14 @@ class Song
   @@all = []
   def initialize(name)
     @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
   end
   
   def artist_name
