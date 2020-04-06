b75b9cef253878f5a27a2ed4e12ce929b58240d8
Justin Winger <justinwinger4@gmail.com>
2020-04-06 16:52:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,31 @@
 class Author 
-  attr_accessor :name, :posts
-  @@post_count = 0 
-  
+  attr_accessor :name
+  @@post_count = 0
+
   def initialize(name)
     @name = name
-    @post = []
+    @posts = []
   end
-  
-  
+
+  def posts
+    @posts
+  end
+
   def add_post(post)
     @posts << post
-    post.author = self 
-    @@post_count += 1 
+    post.author = self
+    @@post_count += 1
   end
-  
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post 
-    post.author = self 
-    @@post_count += 1 
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
-  
+
   def self.post_count
     @@post_count
   end
-  
-  # def posts 
-  #   @posts
-  # end
-end
+
+end 
