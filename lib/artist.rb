c6623dd7afa7980d7fdcf5e1f5f2b8407d0a7ed4
Justin Winger <justinwinger4@gmail.com>
2020-04-06 14:54:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,9 @@ class Artist
     @songs = []
   end
   
-  def add_song(songs)
-    @songs << songs
-    songs.artist = self
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
   
-  def songs
-    @songs
-  end
 end 
