5ac3088a821160f08601a8f7368daab562664f9b
Justin Winger <justinwinger4@gmail.com>
2020-04-06 17:05:31 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,13 @@
 class Post
   attr_accessor :title, :author
+  @@all = []
   def initialize(title)
     @title = title
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
   end
   
   def author_name
